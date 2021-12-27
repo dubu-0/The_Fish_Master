878de497cf77f286b3e004329d9e838c9ef0d5aa
dubu-0 <yudin01@mail.ru>
2021-12-27 18:12:43 +0300

    В TransformExtension.cs вместо 3-х методов сделал 1

--- Assets/Scripts/Core/FishingController.cs
+++ Assets/Scripts/Core/FishingController.cs
@@ -13,13 +13,11 @@ namespace Core
 		[SerializeField] [Range(-30f, -50f)] private float goesUpTo;
 
 		private Coroutine _fishing;
-		private float _fishingLineStartPositionY;
 		private GameParameters _gameParameters;
 		private Camera _main;
 
 		private void Start()
 		{
-			_fishingLineStartPositionY = fishingLine.transform.position.y;
 			_gameParameters = new GameParameters();
 			_main = Camera.main;
 		}
@@ -32,53 +30,43 @@ namespace Core
 
 		private IEnumerator Fishing()
 		{
-			yield return StartCoroutine(MoveDownFast());
+			yield return StartCoroutine(MoveDownFast(100f));
 
 			hook.EnableCollider();
 
-			yield return StartCoroutine(MoveUpSlow());
+			yield return StartCoroutine(MoveUpSlow(8f));
 
 			hook.DisableCollider();
 
-			yield return StartCoroutine(MoveUpFast());
+			yield return StartCoroutine(MoveUpFast(50f));
 
 			hook.ReleaseFishes();
 
 			_fishing = null;
 		}
 
-		private IEnumerator MoveDownFast()
+		private IEnumerator MoveDownFast(float speed)
 		{
 			var offsetY = fishingLine.transform.position.y * 0.35f;
 
-			yield return StartCoroutine
-				(_main.transform.MoveDownFast(fishingLine.transform.position.y + offsetY, 100f, fishingLine.VerticalMovement));
+			yield return StartCoroutine(_main.transform.MoveY(fishingLine.transform.position.y + offsetY, speed));
 
-			StartCoroutine(_main.transform.MoveDownFast(-_gameParameters.Length + offsetY, 100f, fishingLine.VerticalMovement));
-			yield return StartCoroutine(fishingLine.transform.MoveDownFast(-_gameParameters.Length, 100f, fishingLine.VerticalMovement));
+			StartCoroutine(_main.transform.MoveY(-_gameParameters.Length + offsetY, speed));
+			yield return StartCoroutine(fishingLine.transform.MoveY(-_gameParameters.Length, speed));
 		}
 
-		private IEnumerator MoveUpSlow()
+		private IEnumerator MoveUpSlow(float speed)
 		{
-			StartCoroutine
-			(
-				_main.transform.MoveUpSlow
-					(goesUpTo, 0f, fishingLine.VerticalMovement, () => hook.CaughtFishes.Count() >= _gameParameters.Strength)
-			);
-
-			yield return StartCoroutine
-			(
-				fishingLine.transform.MoveUpSlow
-					(goesUpTo, 0f, fishingLine.VerticalMovement, () => hook.CaughtFishes.Count() >= _gameParameters.Strength)
-			);
+			StartCoroutine(_main.transform.MoveY(goesUpTo, speed, () => hook.CaughtFishes.Count() >= _gameParameters.Strength));
+			yield return StartCoroutine(fishingLine.transform.MoveY(goesUpTo, speed, () => hook.CaughtFishes.Count() >= _gameParameters.Strength));
 		}
 
-		private IEnumerator MoveUpFast()
+		private IEnumerator MoveUpFast(float speed)
 		{
-			StartCoroutine(_main.transform.MoveUpFast(_fishingLineStartPositionY, 50f, fishingLine.VerticalMovement));
-			yield return StartCoroutine(fishingLine.transform.MoveUpFast(_fishingLineStartPositionY, 50f, fishingLine.VerticalMovement));
-			yield return StartCoroutine(_main!.transform.MoveUpFast(0f, 50f, fishingLine.VerticalMovement));
-			
+			StartCoroutine(_main.transform.MoveY(fishingLine.StartingPosition.y, speed));
+			yield return StartCoroutine(fishingLine.transform.MoveY(fishingLine.StartingPosition.y, speed));
+			yield return StartCoroutine(_main!.transform.MoveY(0f, speed));
+
 			_main.transform.position = new Vector3(0, 0, _main.transform.position.z);
 		}
 	}
--- Assets/Scripts/FishingGear/FishingLine/FishingLine.cs
+++ Assets/Scripts/FishingGear/FishingLine/FishingLine.cs
@@ -1,14 +1,11 @@
-using Movement;
 using UnityEngine;
 
 namespace FishingGear.FishingLine
 {
 	public sealed class FishingLine : MonoBehaviour
 	{
-		[SerializeField] [Range(1f, 15f)] private float speed;
+		public Vector3 StartingPosition { get; private set; }
 
-		public VerticalMovement VerticalMovement { get; private set; }
-
-		private void Start() => VerticalMovement = new VerticalMovement(speed);
+		private void Start() => StartingPosition = transform.position;
 	}
 }
